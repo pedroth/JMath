73760c70bbda09c33fdb7b661e64e923b1c69623
pedroth <pedrotiago92@gmail.com>
2018-09-17 16:24:00 +0100

    minor change

--- src/test/java/fields/RealTest.java
+++ src/test/java/fields/RealTest.java
@@ -7,8 +7,19 @@ public class RealTest {
 
     @Test
     public void testRealOperations() {
-        Assert.assertTrue(new Real(1).prod(new Real(2)).add(Real.of(1)).sub(Real.of(1)).equals(new Real(2)));
-        Assert.assertTrue(Real.of(1).conj().equals(Real.of(1)));
-        Assert.assertTrue(Real.of(1).symmetric().equals(Real.of(-1)));
+        Assert.assertTrue(new Real(1)
+                             .prod(new Real(2))
+                             .add(Real.of(1))
+                             .sub(Real.of(1))
+                             .equals(new Real(2))
+        );
+        Assert.assertTrue(Real.of(1)
+                          .conj()
+                          .equals(Real.of(1))
+        );
+        Assert.assertTrue(Real.of(1)
+                          .symmetric()
+                          .equals(Real.of(-1))
+        );
     }
 }
